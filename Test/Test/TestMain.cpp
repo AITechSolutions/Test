f767557052a30d6a1ccc7a5493e9c0ae23b6f695
TAbracadabra <lobsterbyter@dejazzd.com>
2014-01-22 23:53:49 -0500

    Added VS project Test and TestMain.cpp

---
+++ Test/Test/TestMain.cpp
@@ -0,0 +1,20 @@
+/*
+* File:   TestMain.cpp
+* Author: TAbracadabra
+*
+* Created on January 22, 2014, 11:41 PM
+*/
+
+#include <iostream>
+
+
+/**
+*
+* @param argc
+* @param argv
+* @return
+*/
+int main(int argc, char** argv)
+{
+	std::cout << "Hi There!" << std::endl;
+}
